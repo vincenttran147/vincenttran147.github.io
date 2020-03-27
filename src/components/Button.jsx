f3fb430def8fce4c29e66049dd70bab8e22ffd43
Vincent Tran <vincenttran147@gmail.com>
2020-03-26 22:34:08 -0500

    Add Header and Button components

---
+++ src/components/Button.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {createUseStyles} from 'react-jss';
+
+
+const styles = createUseStyles({
+  buttonRoot: (props) => ({
+    ...props.props.style,
+    display: 'flex',
+    backgroundColor: props.props.backgroundColor,
+    width: props.props.width,
+    height: props.props.height,
+    borderRadius: props.props.borderRadius,
+    justifyContent: 'center',
+    alignItems: props.props.alignment,
+    transition: props.props.transition,
+    cursor: 'pointer',
+    '&:hover': {
+      backgroundColor: props.props.hoverColor == null ? props.props.backgroundColor : props.props.hoverColor
+    }
+  })
+});
+
+function Button(props) {
+  const classes = styles({props});
+
+  return (
+    <div className={classes.buttonRoot} onMouseOver={props.onMouseOver} onMouseLeave={props.onMouseLeave}>
+      {props.children}
+    </div>
+  );
+}
+
+Button.defaultProps = {
+  width: 50,
+  height: 30,
+  borderRadius: 0,
+  alignment: 'center',
+  backgroundColor: 'lightgray',
+  hoverColor: 'gray',
+  transition: '0.2s'
+}
+
+export default Button;
---
+++ src/components/Header.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { createUseStyles } from 'react-jss';
+import '@fortawesome/fontawesome-free/css/all.css';
+
+import Button from './Button';
+
+
+const styles = createUseStyles({
+  headerRoot: {
+    display: 'flex',
+    backgroundColor: '#878787',
+    height: window.innerHeight,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  openButtonText: {
+    fontFamily: '"Roboto", sans-serif',
+    fontSize: 32
+  },
+  openButtonIconWrapper: (props) => ({
+    marginLeft: 10,
+    transition: '0.2s',
+    transform: `rotate(${props.states.openButtonHover === true ? 90 : 0}deg)`
+  }),
+  openButtonIcon: {
+    fontSize: 32
+  }
+});
+
+function Header() {
+  const [openButtonHover, setOpenButtonHover] = useState(false);
+
+  const classes = styles({
+    states: { openButtonHover }
+  });
+
+  return (
+    <div className={classes.headerRoot}>
+      <Button width={300} height={60} borderRadius={10}
+        onMouseOver={() => setOpenButtonHover(true)} onMouseLeave={() => setOpenButtonHover(false)}>
+        <div className={classes.openButtonText}>View my works</div>
+        <div className={classes.openButtonIconWrapper}>
+          <i className={"fas fa-arrow-right " + classes.openButtonIcon}></i>
+        </div>
+      </Button>
+    </div>
+  );
+}
+
+export default Header;
